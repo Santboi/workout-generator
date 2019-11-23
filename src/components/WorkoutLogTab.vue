4fb740605de32aaf9965d6b01c7a2ee1649a6e87
Dani Galvez <dani.galvez@clubautomation.com>
2019-11-23 16:30:16 -0600

    scaffold data tables for scheduled and logged workouts

--- src/components/WorkoutLogTab.vue
+++ src/components/WorkoutLogTab.vue
@@ -1,8 +1,50 @@
 <template>
-  <h1>this is the workout log tab</h1>
+  <v-simple-table>
+    <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">Date</th>
+            <th class="text-left">Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="workout in scheduledWorkouts"
+            :key="workout.date"
+          >
+            <td>{{ workout.date }}</td>
+            <td>{{ workout.details }}</td>
+          </tr>
+        </tbody>
+      </template>
+  </v-simple-table>
+  
 </template>
 <script>
 export default {
-  name: 'WorkoutLogTab'
+  name: 'WorkoutLogTab',
+  data() {
+    return {
+      workoutScheduleTable: {
+        headers: [
+          {
+            text: 'Date',
+            value: 'date',
+            align: 'start',
+          },
+          {
+            text: 'Details',
+            value: 'details',
+            align: 'start',
+          }
+        ]
+      }
+    }
+  },
+  computed: {
+    scheduledWorkouts() {
+      return [{date: '11/22/2019', details: 'asdf asdf'}, {date: '11/19/2019', details: 'asdf asdf'}, {date: '11/17/2019', details: 'asdf asdf'}]
+    }
+  }
 }
 </script>
--- src/components/WorkoutScheduleTab.vue
+++ src/components/WorkoutScheduleTab.vue
@@ -1,8 +1,50 @@
 <template>
-  <h1>this is the workout schedule tab</h1>
+  <v-simple-table>
+    <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">Date</th>
+            <th class="text-left">Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(workout, i) in scheduledWorkouts"
+            :key="`${workout.date}-${i}`"
+          >
+            <td>{{ workout.date }}</td>
+            <td>{{ workout.details }}</td>
+          </tr>
+        </tbody>
+      </template>
+  </v-simple-table>
+  
 </template>
 <script>
 export default {
-  name: 'WorkoutLogTab'
+  name: 'WorkoutScheduleTab',
+  data() {
+    return {
+      workoutScheduleTable: {
+        headers: [
+          {
+            text: 'Date',
+            value: 'date',
+            align: 'start',
+          },
+          {
+            text: 'Details',
+            value: 'details',
+            align: 'start',
+          }
+        ]
+      }
+    }
+  },
+  computed: {
+    scheduledWorkouts() {
+      return [{date: '11/23/2019', details: 'asdf asdf'}, {date: '11/23/2019', details: 'asdf asdf'}, {date: '11/23/2019', details: 'asdf asdf'}]
+    }
+  }
 }
 </script>
